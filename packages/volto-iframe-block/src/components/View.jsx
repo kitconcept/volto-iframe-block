dfeec6ef6ed22153399fea76794f179503cc7dd5
Jonas Piterek <piterek@kitconcept.com>
2025-02-25 10:37:39 +0100

    fix: only display figure if we have a valid url

--- packages/volto-iframe-block/src/components/View.jsx
+++ packages/volto-iframe-block/src/components/View.jsx
@@ -15,28 +15,28 @@ const IframeView = (props) => {
   return (
     <div className={cx('block iframe align', data.align, className)}>
       <div className="block-container">
-        <figure>
-          {data.src && isValidUrl(data.src) && (
+        {data.src && isValidUrl(data.src) && (
+          <figure>
             <iframe
               src={data.src}
               title={data.title}
               width={width}
               height={data.height}
             />
-          )}
-          <figcaption>
-            {data.title && <div className="title">{data.title}</div>}
-            {data.description && (
-              <div className="description">{data.description}</div>
-            )}
-            {data.credit && (
-              <div className="credit">
-                Credit:{' '}
-                <div dangerouslySetInnerHTML={{ __html: data.credit.data }} />
-              </div>
-            )}
-          </figcaption>
-        </figure>
+            <figcaption>
+              {data.title && <div className="title">{data.title}</div>}
+              {data.description && (
+                <div className="description">{data.description}</div>
+              )}
+              {data.credit && (
+                <div className="credit">
+                  Credit:{' '}
+                  <div dangerouslySetInnerHTML={{ __html: data.credit.data }} />
+                </div>
+              )}
+            </figcaption>
+          </figure>
+        )}
       </div>
     </div>
   );
